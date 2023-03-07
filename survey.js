e39ef2a71e613ab5aadc9acd536e6fb8ec82d8c0
anggr <anggorookky@gmail.com>
2023-03-07 22:07:34 +0800

    add input experience, library, salary

--- survey.js
+++ survey.js
@@ -3,11 +3,11 @@ import inquirer from "inquirer";
 const questions = [
   {
     type: "input",
-    name: "name",
-    message: "What is your name?",
+    name: "firstName",
+    message: "What's your first name?",
     validate: function (value) {
-      if (value.trim().length === 0) {
-        return "Please enter your name.";
+      if (!value) {
+        return "Please enter your first name.";
       }
       return true;
     },
@@ -16,13 +16,71 @@ const questions = [
     type: "input",
     name: "email",
     message: function (answers) {
-      return `Hello, ${answers.name}! What is your email address?`;
+      return `Hello, ${answers.firstName}! What's your email address?`;
     },
     validate: function (value) {
-      // Check if the input is a valid email address
+      if (!value) {
+        return "Please enter your email address.";
+      }
       const valid = /\S+@\S+\.\S+/.test(value);
       if (!valid) {
         return "Please enter a valid email address.";
+      }
+      return true;
+    },
+  },
+  {
+    type: "list",
+    name: "experienced",
+    message: "Are you experienced Developer?",
+    choices: ["Yes", "No"],
+    validate: function (value) {
+      if (!value) {
+        return "Please select your experience level.";
+      }
+      return true;
+    },
+  },
+  {
+    type: "list",
+    name: "yearsOfExperience",
+    message: "How many years of experience do you have with JavaScript?",
+    choices: ["0-1", "1-3", "3-5", "5-10", "10+"],
+    when: function (answers) {
+      return answers.experienced === "Yes";
+    },
+    validate: function (value) {
+      if (!value) {
+        return "Please select your years of experience.";
+      }
+      return true;
+    },
+  },
+  {
+    type: "checkbox",
+    name: "libraries",
+    message: "What JavaScript library do you know?",
+    choices: ["React.js", "Vue", "Angular", "Node.js", "jQuery", "D3.js"],
+    when: function (answers) {
+      return answers.experienced === "Yes";
+    },
+    validate: function (value) {
+      if (!value || value.length === 0) {
+        return "Please select at least one library.";
+      }
+      return true;
+    },
+  },
+  {
+    type: "number",
+    name: "desiredSalary",
+    message: "What is your desired salary?",
+    when: function (answers) {
+      return answers.experienced === "Yes";
+    },
+    validate: function (value) {
+      if (!value || value <= 0) {
+        return "Please enter a salary.";
       }
       return true;
     },
